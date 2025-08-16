e44446d4a0da7211e4759ec356481d104bd8ee28
김주희 <kimjuhee@gimjuhuis-MacBook-Air.local>
2025-08-17 04:05:26 +0900

    Fix: supabse client 수정#47

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,13 @@
 // lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+export function getSupabaseClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase env vars are missing");
+  }
 
-export default supabase;
+  return createClient(supabaseUrl, supabaseAnonKey);
+}
